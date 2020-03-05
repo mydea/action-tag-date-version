506eb8a1ea95d8c853e267f131bdcf78f0217e80
Francesco Novy <francesco@cropster.com>
2020-03-05 17:05:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# action-tag-date-version
+
+This action will create a tag with a date version.
+
+## Example usage
+
+Create a file named `.github/workflows/deploy.yml` in your repo and add the following:
+
+```yaml
+name: ember-cli-code-coverage
+
+on: [pull_request]
+
+jobs:
+  build:
+
+    runs-on: ubuntu-latest
+
+    steps:
+    - uses: actions/checkout@master
+    - uses: mydea/actions-tag-date-version@v1
+      with:
+        repo-token: "${{ secrets.GITHUB_TOKEN }}"
+    - run: deploy-app
+```
+
+Note that the `GITHUB_TOKEN` secret is automatically available, so you don't need to do anything else about that.
---
+++ index.js
@@ -0,0 +1,88 @@
+const { getInput, setFailed } = require('@actions/core');
+const { exec } =  require('@actions/exec');
+const { GitHub } = require('@actions/github');
+const fs = require('fs');
+
+async function run() {
+  try {
+    let myToken = getInput('repo-token', { required: true });
+
+   let octokit = new GitHub(myToken);
+    let pullRequest = await getPullRequest();
+
+    await exec('git fetch --tags');
+    let allTags = await exec('git tag');
+    let versionTags = allTags.map(processVersion).filter(Boolean).sort((a, b) => a.localeCompare(b));
+
+    let nextVersion = getCurrentDateVersion(versionTags[0]);
+
+    console.log(`Next version: ${nextVersion}`);
+
+    // await exec(`git tag ${nextVersion}`);
+    // await exec(`git push ${nextVersion}`);
+
+  } catch (error) {
+    setFailed(error.message);
+  }
+}
+
+run();
+
+function getCurrentDateVersion(latestVersion) {
+  let latestVersionParts = latestVersion.split('.');
+
+  let date = new Date();
+
+  let year = date
+      .getUTCFullYear()
+      .toString()
+      .substr(-2);
+  let month = date.getUTCMonth() + 1;
+
+  let newVersionParts = [`${year}`, `${month}`, '0'];
+
+  if (
+      latestVersionParts[0] === newVersionParts[0] &&
+      latestVersionParts[1] === newVersionParts[1]
+  ) {
+    newVersionParts[2] = latestVersionParts[2] * 1 + 1;
+  }
+
+  return newVersionParts.join('.');
+}
+
+function processVersion(version) {
+  let parts = version.split('.');
+
+  if (parts.length !== 3) {
+    return false;
+  }
+
+  let year = parts[0];
+  let month = parts[1] * 1;
+  let patch = parts[2] * 1;
+
+  if(year.match(/^v\d*/)) {
+    year = year.substr(1);
+  }
+
+  year = year * 1;
+
+  if (Number.isNaN(year) || Number.isNaN(month) || Number.isNaN(patch)) {
+    return false;
+  }
+
+  if (year < 19 || year > 99) {
+    return false;
+  }
+
+  if (month < 1 || month > 12) {
+    return false;
+  }
+
+  if (patch < 0) {
+    return false;
+  }
+
+  return `${year}.${month}.${patch}`
+}
